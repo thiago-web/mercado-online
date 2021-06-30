73fa278e8bcd1a20f7dd0d00b714e196a06639ee
Thiago Henrique <thiagop291@gamil.com>
2021-06-29 21:48:23 -0300

    Pagina do Aplicativo / Menu / Layout - Normalizada

---
+++ mercado/app/carrinho/carrinho.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+</head>
+<body>
+	<div class="container-fluid text-center">
+		<div class="row">
+			<div class="col-xs-8">
+				<div class="panel panel-info">
+					<div class="panel-heading">
+						<div class="panel-title">
+							<div class="row">
+								<div class="col-xs-6">
+									<h5><span class="glyphicon glyphicon-shopping-cart"></span> Carrinho</h5>
+								</div>
+								<div class="col-xs-6">
+									<button type="button" class="btn btn-primary btn-sm btn-block">
+										<span class="glyphicon glyphicon-share-alt"></span> Continuar Comprando
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-xs-2"><img class="img-responsive" src="http://placehold.it/100x70">
+							</div>
+							<div class="col-xs-4">
+								<h4 class="product-name"><strong>Nome do Produto</strong></h4><h4><small>Descrição do Produto</small></h4>
+							</div>
+							<div class="col-xs-6">
+								<div class="col-xs-6 text-right">
+									<h6><strong>25.00 <span class="text-muted">x</span></strong></h6>
+								</div>
+								<div class="col-xs-4">
+									<input type="text" class="form-control input-sm" value="1">
+								</div>
+								<div class="col-xs-2">
+									<button type="button" class="btn btn-link btn-xs">
+										<span class="glyphicon glyphicon-trash"> </span>
+									</button>
+								</div>
+							</div>
+						</div>
+						<hr>
+						<hr>
+						<div class="row">
+							<div class="text-center">
+								<div class="col-xs-9">
+									<h6 class="text-right">Adicionar Itens?</h6>
+								</div>
+								<div class="col-xs-3">
+									<button type="button" class="btn btn-default btn-sm btn-block">
+										Atualizar Carrinho
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="panel-footer">
+						<div class="row text-center">
+							<div class="col-xs-9">
+								<h4 class="text-right">Total <strong>$50.00</strong></h4>
+							</div>
+							<div class="col-xs-3">
+								<button type="button" class="btn btn-success btn-block">
+									Finalizar
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+
+
+
+
--- mercado/app/page-div.js
+++ mercado/app/page-div.js
@@ -5,15 +5,20 @@ function abrir_pag(valor){
     xmlRequest.onreadystatechange = mudancaEstado;
     xmlRequest.send(null);
  
-        if (xmlRequest.readyState == 1) {
-            document.getElementById("conteudo_produtos").innerHTML;
-        }
+    if (xmlRequest.readyState == 1) {
+        document.getElementById("conteudo_produtos").innerHTML;
+    }
  
     return url;
+    
+    
+    
 }
  
 function mudancaEstado(){
     if (xmlRequest.readyState == 4){
         document.getElementById("conteudo_produtos").innerHTML = xmlRequest.responseText;
     }
+document.getElementById("mySidebar").style.width = "0";
+document.getElementById("main").style.marginLeft= "0";
 }
